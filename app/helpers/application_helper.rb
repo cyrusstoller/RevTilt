1faf76afdbee35304a9be612d3bdc04b8edae1c9
Cyrus Stoller <cyrus.stoller@gmail.com>
2013-04-16 17:23:43 -0400

    Added a header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def bootstrap_alert_type(type)
+    case type
+    when :alert, "alert"
+      "error"
+    when :error, "error"
+      "error"
+    when :notice, "notice"
+      "warning"
+    when :success, "success"
+      "success"
+    else
+      type.to_s
+    end
+  end
 end
