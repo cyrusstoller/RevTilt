9e56b88cc5682f0cac482dd96551449b07dd6575
Cyrus Stoller <cyrus.stoller@gmail.com>
2013-04-15 17:20:01 -0700

    Initial commit with new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NuancedReviews::Application.config.secret_token = 'd6a6b620234550a82ec820e7842ca296937a10c1bc8054ba9684bc771d4919055a19a6e96155f7b16838d1e1e4e62f71478ab7e75c33767998c8fb791034254f'
