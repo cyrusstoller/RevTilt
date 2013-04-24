1c032481f9c150f2a8eb4a92111336890946ac54
Cyrus Stoller <cyrus.stoller@gmail.com>
2013-04-24 12:23:58 -0700

    Users should be able to manage their own reviews

--- app/authorization/ability.rb
+++ app/authorization/ability.rb
@@ -10,6 +10,7 @@ class Ability
     #   can :manage, :all
     # end
     
+    can :manage, Review, :user_id => user.id unless user.new_record?
     
     #   if user.admin?
     #     can :manage, :all
--- spec/authorization/ability_spec.rb
+++ spec/authorization/ability_spec.rb
@@ -2,5 +2,25 @@ require 'spec_helper'
 require "cancan/matchers"
 
 describe Ability do
-  
+  describe "Reviews" do
+    describe "guest" do
+      before(:each) do
+        @ability = Ability.new
+      end
+      subject { @ability }
+      
+      it { should_not be_able_to(:manage, FactoryGirl.build(:review)) }
+    end
+    
+    describe "signed in user" do
+      before(:each) do
+        @user = FactoryGirl.create(:user)
+        @ability = Ability.new(@user)
+      end
+      subject { @ability }
+      
+      it { should be_able_to(:manage, FactoryGirl.build(:review, :user => @user)) }
+      it { should_not be_able_to(:manage, FactoryGirl.build(:review, :user_id => @user.id - 1)) }
+    end
+  end
 end
