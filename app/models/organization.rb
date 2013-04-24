13ab6cbcd31529583fbc8b77c903d87e9b4422dc
Cyrus Stoller <cyrus.stoller@gmail.com>
2013-04-23 22:21:04 -0700

    Added a basic iframe to the organization show page

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -18,11 +18,30 @@
 class Organization < ActiveRecord::Base
   attr_accessible :category_id, :latitude, :longitude, :name, :url, :url_type
 
+  # Validations
   validates_presence_of :category_id
   validates_presence_of :name
   validates_presence_of :url
 
+  # Database Relationships
   has_many :organization_user_relationships, :class_name => "Relationships::OrganizationUser", :foreign_key => "organization_id"
   has_many :reviews, :class_name => "Review", :foreign_key => "organization_id"
   has_many :users, :through => :organization_user_relationships, :source => :user
+  
+  # Class Methods
+  def self.category_options
+    return [["activities", 1], 
+            ["childcare", 2], 
+            ["dentist", 3], 
+            ["doctors", 4], 
+            ["haircut / salons", 5], 
+            ["restaurants", 6], 
+            ["schools", 7], 
+            ["therapists", 8]]
+  end
+  
+  # Instance Methods
+  def category_text
+    Organization.category_options[self.category_id - 1][0] rescue "MISC"
+  end
 end
