6fb7d6bf4ef667c7cdb2c71149e3b37c3bc77f50
Cyrus Stoller <cyrus.stoller@gmail.com>
2013-05-11 13:48:26 -0700

    Make it so that the organizations API call can filter based on categories

--- app/controllers/api/v1/organizations_controller.rb
+++ app/controllers/api/v1/organizations_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::OrganizationsController < Api::V1::ApplicationController
   def index
     @organizations = Organization.paginate(:page => params[:page])
-    
+    @organizations = @organizations.where(:category_id => params[:category]) unless params[:category].blank?
     @organizations = @organizations.joins(%(LEFT JOIN "cache_review_stats" ON "cache_review_stats"."organization_id" = "organizations"."id")).
                         select(%("organizations".*, "cache_review_stats"."num_reviews", "cache_review_stats"."avg_review")).
                         where(%("cache_review_stats"."condition_id" = 0 OR "cache_review_stats"."condition_id" IS NULL)).
@@ -11,7 +11,7 @@ class Api::V1::OrganizationsController < Api::V1::ApplicationController
     if params[:latitude].present? and params[:longitude].present? and params[:radius].present?
       @organizations = @organizations.near([params[:latitude], params[:longitude]], params[:radius])
     elsif params[:latitude].present? or params[:longitude].present? or params[:radius].present?
-      render :status => 500, :text => "Invalid API Call"
+      render :status => 500, :text => "Invalid API Call - must provide latitude, longitude, and radius"
     end
   end
   
