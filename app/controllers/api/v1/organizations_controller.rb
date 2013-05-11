34ea68207cc3ab476cf5c2e9362b42a473bb9404
Cyrus Stoller <cyrus.stoller@gmail.com>
2013-05-11 01:18:52 -0700

    Make it so that the API allows restriction based on location and radius

--- app/controllers/api/v1/organizations_controller.rb
+++ app/controllers/api/v1/organizations_controller.rb
@@ -1,6 +1,15 @@
 class Api::V1::OrganizationsController < Api::V1::ApplicationController
   def index
     @organizations = Organization.paginate(:page => params[:page])
+    
+    @organizations = @organizations.joins(%(LEFT JOIN "cache_review_stats" ON "cache_review_stats"."organization_id" = "organizations"."id")).
+                        where(%("cache_review_stats"."condition_id" = 0 OR "cache_review_stats"."condition_id" IS NULL)).
+                        order(%("cache_review_stats"."avg_review" DESC NULLS LAST)).
+                        order(%("cache_review_stats"."num_reviews" DESC))
+    
+    if params[:latitude].present? and params[:longitude].present? and params[:radius]
+      @organizations = @organizations.near([params[:latitude], params[:longitude]], params[:radius])
+    end
   end
   
   def show
