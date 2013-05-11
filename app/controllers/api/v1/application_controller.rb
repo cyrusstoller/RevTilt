029a3f356f7d2f21648a4a563aa6d36619ee0ede
Cyrus Stoller <cyrus.stoller@gmail.com>
2013-05-11 16:29:59 -0700

    Allow multiple origin domains

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,7 +1,15 @@
 class Api::V1::ApplicationController < ActionController::Base
   protect_from_forgery
   
+  before_filter :allow_origin_multiple_origin_domains
+  
   rescue_from CanCan::AccessDenied do |exception|
     render :text => "Access Forbidden", :status => 403, :layout => false
+  end
+  
+  private
+  
+  def allow_origin_multiple_origin_domains
+    response.headers["Access-Control-Allow-Origin"] = "*"
   end
 end
