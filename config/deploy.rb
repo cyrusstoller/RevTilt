217bcfa1418edee531134ef7616301331fc443f5
Cyrus Stoller <cyrus.stoller@gmail.com>
2013-11-17 02:08:44 -0800

    Added basic generated configuration files for Capistrano 3

---
+++ config/deploy.rb
@@ -0,0 +1,44 @@
+set :application, 'revtilt'
+set :repo_url, 'git@github.com:cyrusstoller/RevTilt.git'
+
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+set :deploy_to, '/var/www/revtilt'
+set :scm, :git
+
+set :format, :pretty
+set :log_level, :debug
+set :pty, true
+
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :keep_releases, 10
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.0.0-p247'
+set :bundle_flags, "--deployment" # removing the --quiet flag
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+
+end
