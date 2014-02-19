879aae7f78195591279078b775731bf2b426bc2a
Cyrus Stoller <cyrus.stoller@gmail.com>
2014-02-19 13:37:59 -0800

    Adding public/assets to the linked dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :log_level, :debug
 set :pty, true
 
 set :linked_files, %w{config/database.yml .env}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/assets public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 10
